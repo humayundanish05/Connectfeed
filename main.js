605d788a074b0b7c01385f1365c9e4ce4440f54e
humayundanish05 <humayundanish50@gmail.com>
2025-07-26 00:05:08 +0500

    Update main.js

--- main.js
+++ main.js
@@ -17,10 +17,10 @@
     window.location.href = "home.html";
   });
 
-document.querySelectorAll('.login-box input').forEach(input => {
-    input.addEventListener('click', () => {
-      input.classList.remove('wave-animate'); 
-      void input.offsetWidth; 
-      input.classList.add('wave-animate');
-    });
+document.querySelectorAll(".login-box input").forEach(input => {
+  input.addEventListener("click", () => {
+    input.classList.remove("wave-animate");
+    void input.offsetWidth; // Forces reflow to restart animation
+    input.classList.add("wave-animate");
   });
+});
